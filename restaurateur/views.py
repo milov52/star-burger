edf1703fea0f89042f603235b4fc0bf05ae0d614
killerarz <milov52@gmail.com>
2022-12-20 15:41:39 +0300

    List orders into manager UI.

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -1,14 +1,12 @@
 from django import forms
-from django.shortcuts import redirect, render
-from django.views import View
-from django.urls import reverse_lazy
-from django.contrib.auth.decorators import user_passes_test
-
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views import View
 
-
-from foodcartapp.models import Product, Restaurant
+from foodcartapp.models import Order, Product, Restaurant
 
 
 class Login(forms.Form):
@@ -92,6 +90,7 @@ def view_restaurants(request):
 
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
+    order_items = Order.objects.all()
     return render(request, template_name='order_items.html', context={
-        # TODO заглушка для нереализованного функционала
+        "order_items": order_items,
     })
