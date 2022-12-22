2d506e8cbe66395fadf9cbe3653b8280e0f8836c
killerarz <milov52@gmail.com>
2022-12-22 13:30:55 +0300

    refactoring

--- geo_position/utils.py
+++ geo_position/utils.py
@@ -7,19 +7,22 @@ from star_burger import settings
 # Create your views here.
 def fetch_coordinates(apikey, address):
     base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
+    response = requests.get(
+        base_url,
+        params={
+            "geocode": address,
+            "apikey": apikey,
+            "format": "json",
+        },
+    )
     response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+    found_places = response.json()["response"]["GeoObjectCollection"]["featureMember"]
 
     if not found_places:
         return None
 
     most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    lon, lat = most_relevant["GeoObject"]["Point"]["pos"].split(" ")
     return lon, lat
 
 
@@ -27,7 +30,5 @@ def add_geoposition(address):
     if not GeoPosition.objects.filter(address=address).exists():
         longitude, latitude = fetch_coordinates(settings.API_YANDEX_GEO_KEY, address)
         GeoPosition.objects.create(
-            address=address,
-            longitude=longitude,
-            latitude=latitude
+            address=address, longitude=longitude, latitude=latitude
         )
--- restaurateur/views.py
+++ restaurateur/views.py
@@ -11,36 +11,37 @@ from geopy import distance
 from foodcartapp.models import Order, Product, Restaurant
 from geo_position.models import GeoPosition
 
+
 class Login(forms.Form):
     username = forms.CharField(
-        label='Логин', max_length=75, required=True,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Укажите имя пользователя'
-        })
+        label="Логин",
+        max_length=75,
+        required=True,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Укажите имя пользователя"}
+        ),
     )
     password = forms.CharField(
-        label='Пароль', max_length=75, required=True,
-        widget=forms.PasswordInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Введите пароль'
-        })
+        label="Пароль",
+        max_length=75,
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Введите пароль"}
+        ),
     )
 
 
 class LoginView(View):
     def get(self, request, *args, **kwargs):
         form = Login()
-        return render(request, "login.html", context={
-            'form': form
-        })
+        return render(request, "login.html", context={"form": form})
 
     def post(self, request):
         form = Login(request.POST)
 
         if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
 
             user = authenticate(request, username=username, password=password)
             if user:
@@ -49,75 +50,98 @@ class LoginView(View):
                     return redirect("restaurateur:RestaurantView")
                 return redirect("start_page")
 
-        return render(request, "login.html", context={
-            'form': form,
-            'ivalid': True,
-        })
+        return render(
+            request,
+            "login.html",
+            context={
+                "form": form,
+                "ivalid": True,
+            },
+        )
 
 
 class LogoutView(auth_views.LogoutView):
-    next_page = reverse_lazy('restaurateur:login')
+    next_page = reverse_lazy("restaurateur:login")
 
 
 def is_manager(user):
     return user.is_staff  # FIXME replace with specific permission
 
 
-@user_passes_test(is_manager, login_url='restaurateur:login')
+@user_passes_test(is_manager, login_url="restaurateur:login")
 def view_products(request):
-    restaurants = list(Restaurant.objects.order_by('name'))
-    products = list(Product.objects.prefetch_related('menu_items'))
+    restaurants = list(Restaurant.objects.order_by("name"))
+    products = list(Product.objects.prefetch_related("menu_items"))
 
     products_with_restaurant_availability = []
     for product in products:
-        availability = {item.restaurant_id: item.availability for item in product.menu_items.all()}
-        ordered_availability = [availability.get(restaurant.id, False) for restaurant in restaurants]
+        availability = {
+            item.restaurant_id: item.availability for item in product.menu_items.all()
+        }
+        ordered_availability = [
+            availability.get(restaurant.id, False) for restaurant in restaurants
+        ]
 
-        products_with_restaurant_availability.append(
-            (product, ordered_availability)
-        )
+        products_with_restaurant_availability.append((product, ordered_availability))
 
-    return render(request, template_name="products_list.html", context={
-        'products_with_restaurant_availability': products_with_restaurant_availability,
-        'restaurants': restaurants,
-    })
+    return render(
+        request,
+        template_name="products_list.html",
+        context={
+            "products_with_restaurant_availability": products_with_restaurant_availability,
+            "restaurants": restaurants,
+        },
+    )
 
 
-@user_passes_test(is_manager, login_url='restaurateur:login')
+@user_passes_test(is_manager, login_url="restaurateur:login")
 def view_restaurants(request):
-    return render(request, template_name="restaurants_list.html", context={
-        'restaurants': Restaurant.objects.all(),
-    })
+    return render(
+        request,
+        template_name="restaurants_list.html",
+        context={
+            "restaurants": Restaurant.objects.all(),
+        },
+    )
 
 
 def fetch_coordinates(apikey, address):
     base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
+    response = requests.get(
+        base_url,
+        params={
+            "geocode": address,
+            "apikey": apikey,
+            "format": "json",
+        },
+    )
     response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+    found_places = response.json()["response"]["GeoObjectCollection"]["featureMember"]
 
     if not found_places:
         return None
 
     most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    lon, lat = most_relevant["GeoObject"]["Point"]["pos"].split(" ")
     return lon, lat
 
 
-@user_passes_test(is_manager, login_url='restaurateur:login')
+@user_passes_test(is_manager, login_url="restaurateur:login")
 def view_orders(request):
     order_items = Order.objects.available()
     order_with_restaurants = []
 
     for order in order_items:
         available_restaurants = set()
-        products = [order_products.product for order_products in order.orderproducts.all()]
+        products = [
+            order_products.product for order_products in order.orderproducts.all()
+        ]
         for product in products:
-            availability = [item.restaurant for item in product.menu_items.all() if item.availability]
+            availability = [
+                item.restaurant
+                for item in product.menu_items.all()
+                if item.availability
+            ]
 
             if not available_restaurants:
                 available_restaurants = set(availability)
@@ -127,16 +151,22 @@ def view_orders(request):
         order_coordinates = GeoPosition.objects.coordinates(address=order.address)
         restaurants_with_distance = {}
         for restaurant in available_restaurants:
-            restaurant_coordinates = GeoPosition.objects.coordinates(address=restaurant.address)
+            restaurant_coordinates = GeoPosition.objects.coordinates(
+                address=restaurant.address
+            )
             restaurants_with_distance[restaurant] = round(
                 distance.distance(order_coordinates, restaurant_coordinates).km, 3
             )
 
-        restaurants_with_distance = dict(sorted(restaurants_with_distance.items(), key=lambda item: item[1]))
-        order_with_restaurants.append(
-            (order, restaurants_with_distance)
+        restaurants_with_distance = dict(
+            sorted(restaurants_with_distance.items(), key=lambda item: item[1])
         )
+        order_with_restaurants.append((order, restaurants_with_distance))
 
-    return render(request, template_name='order_items.html', context={
-        "order_items": order_with_restaurants,
-    })
+    return render(
+        request,
+        template_name="order_items.html",
+        context={
+            "order_items": order_with_restaurants,
+        },
+    )
