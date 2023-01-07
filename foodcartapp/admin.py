2c0bacda38a5f9b13b59077c5be73351e98915f2
killerarz <milov52@gmail.com>
2023-01-07 23:17:01 +0300

    fixed a long line

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -134,7 +134,9 @@ class ProductAdmin(admin.ModelAdmin):
         if not obj.image or not obj.id:
             return 'нет картинки'
         edit_url = reverse('admin:foodcartapp_product_change', args=(obj.id,))
-        return format_html('<a href="{edit_url}"><img src="{src}" style="max-height: 50px;"/></a>', edit_url=edit_url, src=obj.image.url)
+        return format_html('<a href="{edit_url}"><img src="{src}" style="max-height: 50px;"/></a>',
+                           edit_url=edit_url,
+                           src=obj.image.url)
     get_image_list_preview.short_description = 'превью'
 
 
